89b479ac8cb41ad2df247fbc28aac1061416c3a8
Sophie <57460242+sophiechat@users.noreply.github.com>
2020-03-31 09:27:23 -0300

    Update iframe.js

--- iframe.js
+++ iframe.js
@@ -1,21 +1,24 @@
   $(document).ready( function() {
 
-            //$( ".janela" ).load( "Chattermodal.html?sessionId=63679615894351484655386207");
-        
+             var _chatterURL = 'https://assistant.sophie.chat/NOMEDOSEUBOT/ChatterWeb/index?login=visitante';
 
-            $(".fa-comment").click(function(){
+            //Open
+            $(".fa-comment").click(function () {
+                $(".janela-modal iframe").attr("src", _chatterURL)
                 var janela = $(".janela-modal");
-                janela.stop(true,false).fadeIn();
-                $(".fa-comment").stop(true,false).fadeOut();
-                $(".fa-times-circle").stop(true,false).fadeIn();
+                janela.stop(true, false).fadeIn();
+                $(".fa-comment").stop(true, false).fadeOut();
+                $(".fa-times-circle").stop(true, false).fadeIn();
                 $(".bubble-ola").hide();
             });
-            $(".fa-times-circle").click(function(){
+
+            //Close
+            $(".fa-times-circle").click(function () {
+                $(".janela-modal iframe").removeAttr("src");
                 var janela = $(".janela-modal");
-                janela.stop(true,false).fadeOut();
-                $(".fa-comment").stop(true,false).fadeIn();
-                $(".fa-times-circle").stop(true,false).fadeOut();
-                /* $(".bubble-nps").show(); retirado as carinhas */
+                janela.stop(true, false).fadeOut();
+                $(".fa-comment").stop(true, false).fadeIn();
+                $(".fa-times-circle").stop(true, false).fadeOut();
             });
 
             $(".bubble-ola").delay(1000).stop(true,false).fadeIn(500);
